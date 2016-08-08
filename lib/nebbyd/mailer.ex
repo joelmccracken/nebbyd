0f0535d948975ce474681a8552a9014bcd3881e2
Joel McCracken <mccracken.joel@gmail.com>
2016-08-08 00:09:06 -0400

    maybe?

--- lib/nebbyd/mailer.ex
+++ lib/nebbyd/mailer.ex
@@ -1,7 +1,5 @@
 defmodule Nebbyd.Mailer do
-  use Mailgun.Client,
-    domain: Application.get_env(:nebbyd, :mailgun_domain),
-    key: Application.get_env(:nebbyd, :mailgun_key)
+  use Mailgun.Client, domain: Application.get_env(:nebbyd, :mailgun_domain), key: Application.get_env(:nebbyd, :mailgun_key)
 
   def send_welcome_text_email(email_address) do
     send_email to: email_address, from: "us@example.com", subject: "Welcome!", text: "Welcome to HelloPhoenix!"
